4e8a8b1266c1abf2d94a3a6552fa505c1aaec632
Yash Panchal <yashp.2920@gmail.com>
2020-11-25 22:30:33 +0530

    login page added

--- studentsetu_project/studentsetu_app/urls.py
+++ studentsetu_project/studentsetu_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.function1,name='function1')
+    path('',views.loginpage,name='loginpage')
 ]
--- studentsetu_project/studentsetu_app/views.py
+++ studentsetu_project/studentsetu_app/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 # Create your views here.
 
-def function1(request):
-    return render(request, 'index.html')
+def loginpage(request):
+    return render(request, 'login.html')
