f4ddab66fe02ab16b512e544907ea7fecde2d3ae
Yash Panchal <yashp.2920@gmail.com>
2020-11-26 15:46:48 +0530

    project del

--- studentsetu_project/studentsetu_app/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse, JsonResponse
-# Create your views here.
-
-def loginpage(request):
-    return render(request, 'login.html')
